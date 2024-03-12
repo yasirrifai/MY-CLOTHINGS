a1a0fd97c6c18d118236e6e83cde61964ce01b3d
yasir rifai <yasirrifai30@gmail.com>
2024-03-12 23:27:57 +0530

    Initial Commit

---
+++ MY-CLOTHINGS/MY_CLOTHINGSApp.swift
@@ -0,0 +1,17 @@
+//
+//  MY_CLOTHINGSApp.swift
+//  MY-CLOTHINGS
+//
+//  Created by Yasir Rifai on 2024-03-12.
+//
+
+import SwiftUI
+
+@main
+struct MY_CLOTHINGSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
